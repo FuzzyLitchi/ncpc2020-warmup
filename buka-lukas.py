23e45612772d0b1ace2aee60f11c5eabb2e99751
Lukires <lukas.thomas.schilling@gmail.com>
2020-10-31 11:18:58 +0100

    Solved buka

---
+++ buka-lukas.py
@@ -0,0 +1,15 @@
+import sys
+
+
+data = []
+
+for line in sys.stdin:
+    data.append(line.split()[0])
+
+a = int(data[0])
+operator = str(data[1])
+b = int(data[2])
+if (operator == "*"):
+    print(str(a*b))
+if (operator == "+"):
+    print(str(a+b))
